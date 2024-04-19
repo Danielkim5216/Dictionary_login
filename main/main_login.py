0e95da72d999ce2bc7418f0f286d4f5846e53686
Danielkim <danielkim5216@gmail.com>
2024-04-19 22:02:25 +0900

    feat: check id and pw in login.py

--- main/main_login.py
+++ main/main_login.py
@@ -9,18 +9,25 @@ class User:
         pass
 
     def account_search(self):
-        findaccout  = (item for item in account_list if item['userid'] == self.userid and item['userpw'] == self.userpw)
-        check = (next(findaccout,False))
-        if check == False:
-            print("no account!")
+        findid = (item for item in account_list if item['userid'] == self.userid)
+        checkid = (next(findid,False))
+        if checkid == False:
+            print(f'cannot find "{self.userid}!"')
         else:
-            print(f'login is success! hello "{self.userid}"')
+            findpw= (item for item in account_list if item['userpw'] == self.userpw)
+            checkpw = (next(findpw,False))
+            if checkpw == False:
+                print("Password is Wrong!")
+            else:
+                print(f'login is success! hello "{self.userid}"')
 
     def root_login(userid,userpw):
         if userid == "root" and userpw == "root":
             print("root login success!")
         else:
             print("login Fail")
+            
+
             '''checkroot = (item for item in account_list if item['userid'] == userid == 'root' and item['userpw'] == userpw == 'root')
         check = (next(checkroot,False))
         if check == False:
